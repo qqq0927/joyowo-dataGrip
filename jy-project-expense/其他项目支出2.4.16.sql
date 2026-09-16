ecf5c2affe94377c3cda16b53b7b6a6f27068402
qqq <qiuqq7383@joyowo.com>
2026-09-16 11:31:12 +0800

    feat(database): 添加加班餐费类型数据到费用类型表
    - 在fee_type_item表中插入新的费用类型项目
    - 添加ID为40的加班餐费记录
    - 设置类型ID为4，类型名称为'其他'
    - 配置状态为启用，排序值为35
    - 设置创建时间为当前时间

---
+++ jy-project-expense/其他项目支出2.4.16.sql
@@ -0,0 +1,18 @@
+INSERT INTO fee_type_item (
+    id,
+    type_id,
+    type_name,
+    item_name,
+    status,
+    sort,
+    create_time
+)
+VALUES (
+    40,
+    4,
+    '其他',
+    '加班餐费',
+    1,
+    35,
+    NOW()
+);
